5c860c7116da871f37355c4c9644dc9be1aed208
GeoLiang90 <gliang90@stuy.edu>
2019-01-15 17:58:25 -0500

    Added mafia

---
+++ mafia.h
@@ -0,0 +1,11 @@
+#ifndef MAFIA_H
+#define MAFIA_H
+#include <time.h>
+#include "player.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+int run_game(int * socket_list);
+
+#endif
