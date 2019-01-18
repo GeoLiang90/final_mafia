8b4655142d3cb0941c58354695d6c1a731c4f6b8
Sajed Nahian <snahian@stuy.edu>
2019-01-18 17:35:14 -0500

    Add files via upload

--- client.c
+++ client.c
@@ -1,8 +1,15 @@
 #include "networking.h"
 #include "mafia.h"
 //
+char role[20];
+char name[20];
+int server_socket;
+char buff[BUFFER_SIZE];
+int sent_name = 0;
+int roster = 0;
+int day = 0;
+
 int send_name(int serv_sock){
-  char name[20];
   printf("Please enter your nickname: ");
   fgets(name, 20, stdin);
   write(serv_sock,name,20);
@@ -14,12 +21,6 @@ void send_msg(int serv_sock, char * msg){
 }
 
 int main(int argc, char **argv) {
-  int server_socket;
-  char buff[BUFFER_SIZE];
-  int sent_name = 0;
-  int roster = 0;
-  int day = 0;
-  char role[20];
   printf("[Client] Waiting for connection to server \n");
   server_socket = client_setup(TEST_IP);
   read(server_socket,buff,BUFFER_SIZE);
@@ -27,8 +28,8 @@ int main(int argc, char **argv) {
   if (strstr(buff,"Mafia")){
     strcpy(role,"Mafia");
   }
-  else if(strstr(buff,"Serial Killer")){
-    strcpy(role,"Serial Killer");
+  else if(strstr(buff,"Doctor")){
+    strcpy(role,"Doctor");
   }
   else{
     strcpy(role, "Town Member");
@@ -57,14 +58,36 @@ int main(int argc, char **argv) {
       roster += 1;
     }
     if (strcmp(state_read,"g2")==0){
-      day += 1;
-      printf("The sun has risen and it's time to wake up");
-      printf("Days Past: %d \n",day);
-      //Place in code to listen for user input and then based on that input, stuff will either be displayed as a chat or, votes can be conducted, proceed can be made
+      dayTime();
     }
     if (strcmp(state_read,"g2")==0){
       //Depends on the players role.
     }
 
   }
+}
+
+int getData(int serv_sock){
+  fgets(name, 20, stdin);
+  write(serv_sock,name,20);
+  return 1;
+}
+
+void dayTime () {
+      day += 1;
+      printf("The sun has risen and it's time to wake up\n");
+      printf("---------------------------\n");
+      printf("Days Past: %d \n",day);
+      printf("---------------------------\n");
+      if (strcmp(role, "Doctor") == 0) {
+	       printf("Which person would you like to heal?: ");
+         getData();
+      }
+      if (strcmp(role, "Mafia") == 0) {
+	       printf("Which person would you like to kill?: ");
+         getData();
+      }
+      if (strcmp(role, "Town Member") == 0) {
+	       printf("<Town Members cannot perform any actions during night>\n");
+      }
 }
--- mafia.c
+++ mafia.c
@@ -14,26 +14,25 @@ int check_win(struct Player * p_list){
     }
     if (life_check == (PLAYER_COUNT - 2)){
       int count_mafia = 0;
-      int count_sk = 0;
       int count_town = 0;
       for(int z = 0; z < PLAYER_COUNT; z++){
-        if (((&p_list[z]) -> isalive == 1) && (strcmp((&p_list[z]) -> role,"town") == 0)){
+        if (((&p_list[z]) -> isalive == 1) && (strcmp((&p_list[z]) -> role, "town") == 0)){
           count_town += 1;
         }
-        if (((&p_list[z]) -> isalive == 1) && (strcmp((&p_list[z]) -> role,"sk") == 0)){
+        if (((&p_list[z]) -> isalive == 1) && (strcmp((&p_list[z]) -> role, "doctor") == 0)){
+          count_town += 1;
+        }
+        if (((&p_list[z]) -> isalive == 1) && (strcmp((&p_list[z]) -> role, "mafia") == 0)){
           count_mafia += 1;
         }
-        if (((&p_list[z]) -> isalive == 1) && (strcmp((&p_list[z]) -> role,"mafia") == 0)){
-          count_sk += 1;
-        }
       }
-      if (count_mafia == 0 && count_sk == 0){
+      if (count_mafia == 0){
         return T_WIN;
       }
       if (count_mafia == 0 && count_town ==0){
         return S_WIN;
       }
-      if (count_sk == 0 && count_town == 0){
+      if (count_town == 0){
         return M_WIN;
       }
     }
@@ -56,10 +55,10 @@ void assign_roles(int * socket_list, struct Player * p_list){
     }
     else if (pop_count == 1){
       char buff[250];
-      strcpy(buff,"You are assigned the role and alliance: Serial Killer  \n");
+      strcpy(buff,"You are assigned the role and alliance: Doctor  \n");
       (&p_list[pop_count])-> socket_fd = socket_list[pop_count];
       (&p_list[pop_count])-> isalive = 1;
-      strcpy((&p_list[pop_count])-> role, "sk");
+      strcpy((&p_list[pop_count])-> role, "doctor");
       (&p_list[pop_count])-> perms = 1;
       write(socket_list[pop_count],buff,sizeof(buff));
     }
@@ -128,12 +127,11 @@ int run_game(int * socket_list){
   //Player Structs go here
   struct Player * player_list = (struct Player *) calloc(PLAYER_COUNT,sizeof(struct Player));
   assign_roles(socket_list,player_list);
-  sleep(2);
+  sleep(1);
   while(1){
   if(game_state == 0){
     for (int i = 0; i < PLAYER_COUNT; i++){
       //Basically tell the client to be ready to send stuff
-	printf("running\n");
         write(socket_list[i], "g0", 5);
     }
     assign_name(socket_list,player_list);
@@ -156,11 +154,13 @@ int run_game(int * socket_list){
     if (game_state == 1){
       printf("Pre Game: Sending Roster to all \n");
       char roster[256];
-      strcpy(roster,"Current Roster: \n");
+      strcpy(roster,"\nCurrent Roster: \n");
       for (int i = 0; i < PLAYER_COUNT; i++){
+	strcat(roster,"\t-");
         strcat(roster,((&player_list[i])-> nickname));
         strcat(roster,"\n");
       }
+      strcat(roster,"\n");
       printf("%s \n", roster);
       //These do not work properly until you fix write_client
       /*
@@ -177,7 +177,7 @@ int run_game(int * socket_list){
     if(game_state == 2){
       for (int i = 0; i < PLAYER_COUNT; i++){
         write(socket_list[i],"g2",5);
-        }
+      }
       break;
       //Course through the list of player and check isDead then list who is connected
       if (check_win(player_list) != 0){
